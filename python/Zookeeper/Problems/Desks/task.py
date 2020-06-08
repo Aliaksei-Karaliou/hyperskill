cf00e99f0f4cbc80cfc7bc2085d4ec9dbb0cb6d5
Aliaksei Karaliou <alexeykoroliov@gmail.com>
2020-06-08 17:28:08 +0300

    zookeeper

---
+++ python/Zookeeper/Problems/Desks/task.py
@@ -0,0 +1,7 @@
+# put your python code here
+
+class1 = int(input())
+class2 = int(input())
+class3 = int(input())
+
+print((class1 + 1) // 2 + (class2 + 1) // 2 + (class3 + 1) // 2)
---
+++ python/Zookeeper/Problems/Difference of times/task.py
@@ -0,0 +1,10 @@
+# put your python code here
+
+hours1 = int(input())
+minutes1 = int(input())
+seconds1 = int(input())
+hours2 = int(input())
+minutes2 = int(input())
+seconds2 = int(input())
+
+print(3600 * (hours2 - hours1) + 60 * (minutes2 - minutes1) + seconds2 - seconds1)
