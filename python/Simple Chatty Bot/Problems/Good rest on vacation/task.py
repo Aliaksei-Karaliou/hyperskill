b7700f587cd404b6e6ea0c51e79a528620836ef5
Aliaksei Karaliou <alexeykoroliov@gmail.com>
2020-07-03 02:19:50 +0300

    Simple Chatty Bot

---
+++ python/Simple Chatty Bot/Problems/Good rest on vacation/task.py
@@ -0,0 +1,8 @@
+# put your python code here
+
+duration = int(input())
+food = int(input())
+flight = int(input())
+hotel = int(input())
+
+print(2 * flight + duration * food + (duration - 1) * hotel)
